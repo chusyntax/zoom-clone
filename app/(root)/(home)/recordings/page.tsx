8b023f1063dd375e83941d3b051f3899fe46444c
Chu <ttheko101@gmail.com>
2024-06-20 00:32:51 +0200

    Completed Cleck Auth

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const PersonalRoom = () => {
+const Recordings = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
     <h1 className="text-3xl font-bold">
-        Personal Room
+    Recordings
     </h1>
    </section>
   )
 }
 
-export default PersonalRoom
+export default Recordings
